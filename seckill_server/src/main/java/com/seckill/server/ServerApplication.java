c22d81c9ebd10e385ee94e7df007784e76c4566d
chliu65 <chang_liu@hnu.edu.cn>
2020-06-13 17:21:49 +0800

    613

---
+++ seckill_server/src/main/java/com/seckill/server/ServerApplication.java
@@ -0,0 +1,20 @@
+package com.seckill.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Auther: wdd
+ * @Date: 2019/9/7 22:01
+ * @Description:
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class ServerApplication {
+    //服务端
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class,args);
+    }
+}
+
